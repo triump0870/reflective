cf1dbe497bca5a200a6805013c11f0140bfc10dd
Rohan <b4you0870@gmail.com>
2019-01-18 22:39:49 +0530

    [Backend] Add the signal in the user app to create employee instance on creation on user instance

--- reflective/users/signals.py
+++ reflective/users/signals.py
@@ -1,13 +1,13 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from reflective.employees.models import Employee
-from .models import User
+from reflective.users.models import User
 
 
 @receiver(post_save, sender=User)
 def create_employee_for_user(sender, instance, created, **kwargs):
     if not created:
         return
-
-    employee = Employee.objects.create()
-    employee.user = instance
+    from reflective.employees.models import Employee
+    Employee.objects.create(
+        user=instance
+    )
