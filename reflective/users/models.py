0cb463fba1a51278a447337bc22675eb518d8306
Rohan <b4you0870@gmail.com>
2019-01-18 20:09:04 +0530

    [Backend] Update the User model

--- reflective/users/models.py
+++ reflective/users/models.py
@@ -5,10 +5,12 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
+
+    def __str__(self):
+        return self.name
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
