3b302bcb7d911e36053797527cbb948ba67538ae
Rohan <b4you0870@gmail.com>
2019-01-18 22:35:14 +0530

    [Backend] Add EmployeeStackedAdmin

--- reflective/employees/admin.py
+++ reflective/employees/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Employee, Department, EffectiveDate
 
+
 # Register your models here.
+class EmployeeStackedAdmin(admin.StackedInline):
+    model = Employee
+
+
 admin.site.register(Employee)
 admin.site.register(Department)
 admin.site.register(EffectiveDate)
